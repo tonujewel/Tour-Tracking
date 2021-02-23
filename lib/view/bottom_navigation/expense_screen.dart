5fe47877912ecc517784a8caad8fc7d273cfe45b
Jewel Rana <tonujewel@gmail.com>
2021-02-23 14:26:37 +0600

    expense

--- lib/view/bottom_navigation/expense_screen.dart
+++ lib/view/bottom_navigation/expense_screen.dart
@@ -14,22 +14,8 @@ class ExpenseScreen extends StatefulWidget {
 
 class _ExpenseScreenState extends State<ExpenseScreen> {
 
-  double totalAmount;
-  @override
-  void initState() {
-   queryValues();
-    super.initState();
-  }
 
-  double  queryValues(String id) {
-    FirebaseFirestore.instance.collectionGroup('expense$id').snapshots().listen((snapshot) {
-      double tempTotal = snapshot.docs.fold(0, (tot, doc) => tot + double.parse(doc['item_price']));
 
-      print("$tempTotal");
-      return  tempTotal;
-
-    });
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,10 +56,7 @@ class _ExpenseScreenState extends State<ExpenseScreen> {
                             "${doc["title"]}",
                             style: TextStyle(fontSize: 18, letterSpacing: 2, color: Style.textColor),
                           ),
-                          Text(
-                            "${queryValues(doc["trip_id"])}",
-                            style: TextStyle(fontSize: 18, letterSpacing: 2, color: Style.textColor),
-                          ),
+
                         ],
                       ),
                     ),
