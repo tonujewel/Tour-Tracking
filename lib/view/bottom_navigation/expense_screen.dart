e0186bbd634273b8f239d2ec4c1e26e93acdea52
jewelrana <tonujewel@gmail.com>
2021-02-25 19:43:02 +0600

    expense added

--- lib/view/bottom_navigation/expense_screen.dart
+++ lib/view/bottom_navigation/expense_screen.dart
@@ -9,7 +9,9 @@ class ExpenseScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Expense") ,),
+      appBar: AppBar(
+        title: Text("Expense"),
+      ),
       body: new StreamBuilder<QuerySnapshot>(
           stream: FirebaseFirestore.instance.collectionGroup('trip_list${prefs.getString("uid")}').snapshots(),
           builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
@@ -21,36 +23,39 @@ class ExpenseScreen extends StatelessWidget {
 
   getExpenseItems(AsyncSnapshot<QuerySnapshot> snapshot) {
     return snapshot.data.docs
-        .map((doc) => Container(
-      child: Row(
-        children: [
-          Expanded(
-            child: GestureDetector(
-              onTap: () => Get.to(AddExpense(tripID: doc["trip_id"].toString(),)),
-              child: Container(
-                  padding: EdgeInsets.only(left: 15, top: 6, bottom: 6),
-                  margin: EdgeInsets.only(left: 15, top: 10, right: 15),
-                  decoration: BoxDecoration(
-                    color: Color(0xFFE3E8F0),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "${doc["title"]}",
-                        style: TextStyle(fontSize: 18, letterSpacing: 2, color: Style.textColor),
+        .map(
+          (doc) => Container(
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () => Get.to(AddExpense(
+                      tripID: doc["trip_id"].toString(),
+                    )),
+                    child: Container(
+                      padding: EdgeInsets.only(left: 15, top: 6, bottom: 6),
+                      margin: EdgeInsets.only(left: 15, top: 10, right: 15),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFE3E8F0),
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
-                    ],
-                  )),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "${doc["title"]}",
+                            style: TextStyle(fontSize: 18, letterSpacing: 2, color: Style.textColor),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
-    ))
+        )
         .toList();
   }
 }
 
-
-// onTap: () => Get.to(AddExpense(tripID: doc["trip_id"].toString(),)),
